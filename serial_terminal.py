0a1aee5d5ca01e76511e412ff91a6a92c7c256bf
Yilmaz Eyidogan <yilmaz.eyidogan@gmail.com>
2026-04-28 22:06:05 +0300

    added rx msg

--- serial_terminal.py
+++ serial_terminal.py
@@ -2,6 +2,7 @@ import serial
 import serial.tools.list_ports
 import keyboard
 import threading
+import sys
 
 PORT = 'COM25' 
 BAUD = 115200
@@ -12,12 +13,23 @@ for port in ports:
 
 try:
     ser = serial.Serial(PORT, BAUD, timeout=0.1)
-    print(f"--- {PORT} connected. Write and press Enter ---")
+    print(f"--- Connected to {PORT}. Press ESC to exit ---")
 except:
     print(f"{PORT} cannot opened!")
-    exit()
+    sys.exit()
 
 tx_msg = ""
+
+def read_from_serial():
+    global tx_msg
+    while ser.is_open:
+        if ser.in_waiting > 0:
+            data = ser.read(ser.in_waiting).decode(errors='replace')
+            print(f"\r[RX]: {data.strip()}")
+            print(f"> {tx_msg}", end="", flush=True)
+
+thread_rx = threading.Thread(target=read_from_serial, daemon=True)
+thread_rx.start()
 
 def on_key_event(e):
     global tx_msg
@@ -29,12 +41,16 @@ def on_key_event(e):
         elif e.name == 'enter':
             ser.write((tx_msg + '\r\n').encode())
             tx_msg = ""
+            print("\n> ", end="", flush=True)
         elif e.name == 'backspace':
             tx_msg = tx_msg[:-1]
-            print(f"\r> {tx_msg} ", end="", flush=True)
-        elif len(e.name) > 0:
+            print("\b \b", end="", flush=True)
+        elif len(e.name) == 1:
             tx_msg += e.name
             print(e.name, end="", flush=True)
+        elif e.name == 'space':
+            tx_msg += " "
+            print(" ", end="", flush=True)
 
 keyboard.hook(on_key_event)
 
