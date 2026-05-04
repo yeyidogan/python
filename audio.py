7ec08d3fc2275a23026d69c8a5aebde27c64c354
Yilmaz Eyidogan <yilmaz.eyidogan@gmail.com>
2026-05-04 23:48:11 +0300

    yeni sesler

--- audio.py
+++ audio.py
@@ -3,16 +3,11 @@ import edge_tts
 
 # STM32 projen için 10 adet anons
 anonslar = {
-    "0001": "Kapı açılıyor.",
-    "0002": "Kapı kapandı.",
-    "0003": "Lütfen kartınızı okutunuz.",
-    "0004": "Geçersiz kart, giriş reddedildi.",
-    "0005": "Sistem aktif hale getirildi.",
-    "0006": "Giriş başarılı, hoş geldiniz.",
-    "0007": "Lütfen bekleyiniz, işlem yapılıyor.",
-    "0008": "Hatalı şifre girdiniz.",
-    "0009": "Dikkat! Güvenlik alarmı devreye girdi.",
-    "0010": "Sistem kapatılıyor, iyi günler."
+    "001": "Hoşgeldiniz.",
+    "002": "Kapı açılıyor.",
+    "003": "Kapı kilitleniyor.",
+    "004": "Kapı kapanıyor.",
+    "005": "Dolu.",
 }
 
 async def generate_anonslar():
@@ -20,7 +15,7 @@ async def generate_anonslar():
         # Kadın sesi için Emel, erkek sesi için Ahmet seçebilirsin
         voice = "tr-TR-EmelNeural" 
         
-        communicate = edge_tts.Communicate(text, voice, rate="+10%")
+        communicate = edge_tts.Communicate(text, voice, rate="+2%")
         await communicate.save(f"{num}.mp3")
         print(f"Oluşturuldu: {num}.mp3 -> {text}")
 
