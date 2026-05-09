5d7d697445a62ae6ef567bd916b7ee160f5c85a9
Yilmaz Eyidogan <yilmaz.eyidogan@gmail.com>
2026-05-10 00:22:55 +0300

    electroist calismalari

--- audio.py
+++ audio.py
@@ -1,23 +1,44 @@
 import asyncio
 import edge_tts
+import os
 
-# STM32 projen için 10 adet anons
 anonslar = {
     "001": "Hoşgeldiniz.",
     "002": "Kapı açılıyor.",
     "003": "Kapı kilitleniyor.",
-    "004": "Kapı kapanıyor.",
+    "004": "Kapı kapanıyor. Kapıyı kilitlemek için lütfen sensöre dokununuz",
     "005": "Dolu.",
 }
 
-async def generate_anonslar():
+voices = {
+    "001": "Welcome.",
+    "002": "Door opening.",
+    "003": "Door locking",
+    "004": "Door closing. Touch the sensor to lock the door.",
+    "005": "Full.",
+}
+
+async def generate_tr_anonslar():
     for num, text in anonslar.items():
-        # Kadın sesi için Emel, erkek sesi için Ahmet seçebilirsin
-        voice = "tr-TR-EmelNeural" 
+        voice = "tr-TR-EmelNeural"
         
+        if not os.path.exists("01"):
+            os.makedirs("01")
         communicate = edge_tts.Communicate(text, voice, rate="+2%")
-        await communicate.save(f"{num}.mp3")
-        print(f"Oluşturuldu: {num}.mp3 -> {text}")
+        await communicate.save(f"01/{num}.mp3")
+        print(f"Generated: {num}.mp3 -> {text}")
+
+async def generate_en_voices():
+    for num, text in voices.items():
+        voice = "en-US-AriaNeural"
+        
+        if not os.path.exists("02"):
+            os.makedirs("02")
+        communicate = edge_tts.Communicate(text, voice, rate="+2%")
+        await communicate.save(f"02/{num}.mp3")
+        print(f"Generated: {num}.mp3 -> {text}")
+
 
 if __name__ == "__main__":
-    asyncio.run(generate_anonslar())
+    asyncio.run(generate_tr_anonslar())
+    asyncio.run(generate_en_voices())
